57284ad66ff788c1f9953cb061f721a92a2daf7f
ASUS_kwpinthong <kw.pinthong@gmail.com>
2017-05-16 18:49:25 +0700

    fix1

---
+++ form1.php
@@ -0,0 +1,20 @@
+<?php require_once("header.php"); ?>
+
+	<!-- MAIN SECTION -->
+	<div class="w3-white">
+			<div class="w3-container w3-content w3-padding-64" style="max-width:650px">
+				<h2 class="w3-wide w3-center">STUDENT's 1st Year Form</h2>
+				<p><b>Student's ID</b></p>
+				<input class="w3-input w3-border" type="text" placeholder="Student's ID" required name="student_id">
+				<p><b>First Name</b></p>
+				<input class="w3-input w3-border" type="text" placeholder="First name" required name="firstname">
+				<p><b>Last Name</b></p>
+				<input class="w3-input w3-border" type="text" placeholder="Last name" required name="lastname">
+				<p><b>จบจากโรงเรียน</b></p>
+				<p><b>เข้ามหาวิทยาลัยเชียงใหม่โดย</b></p>
+				<p><b>แนะนำตัวเอง</b></p>
+				<p><b>ใส่รูปภาพ</b></p>
+			</div>
+	</div>
+		
+<?php require_once("footer.php"); ?>
