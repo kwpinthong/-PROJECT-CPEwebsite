3726ef812204a0393bebb4436909aad89078c549
JPsupiya <Jarupron626@gmail.com>
2017-05-16 23:41:39 +0700

    about us

--- about.php
+++ about.php
@@ -2,10 +2,11 @@
 
 	<!-- Header with full-height image -->
 		<header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
-			<div class="w3-display-left w3-text-white" style="padding:48px">
-				<span class="w3-jumbo w3-hide-small">Start something that matters</span><br>
+			<div class="w3-display-Topleft w3-text-white" style="padding:48px">
+				<span class="w3-jumbo w3-hide-small"><b>Department of Computer Engineering </font></b></span><br>
 				<span class="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br>
-				<span class="w3-large">Stop wasting valuable time with projects that just isn't you.</span>
+				<span class="w3-large"><font size = "8"> ChiangMai University </font></span>
+				
 			</div> 
 			<div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
 				<i class="fa fa-facebook-official w3-hover-opacity"></i>
@@ -19,8 +20,53 @@
 
 	<!--CONTENT-->
 		<div class="w3-container" style="padding:128px 16px" id="about">
-			<h3 class="w3-center">ABOUT THE COMPANY</h3>
-			<p class="w3-center w3-large">Key features of our company</p>
+			<h3 class="w3-center"></h3>
+			<p class="w3-center w3-large"> </p>
+			<CENTER><font size = "5"> ABOUT US </font></CENTER>
+			
+			 <BODY>
+			    <p class="w3-center w3-large"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ภาควิชาวิศวกรรมคอมพิวเตอร์
+				คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่ ก่อตั้งขึ้นเมื่อปี พ.ศ. 2536 จนถึงปัจจุบันได้ผลิตวิศวกรที่เป็นที่ยอมรับของ<br>
+				ทั้งภาครัฐและเอกชนออกไปทำงานในหลายสาขา เช่น วิศวกรระบบเครือข่าย นักพัฒนาโปรแกรมและนักออกแบบระบบอัตโนมัติในงานอุตสาหกรรม เป็นต้น<br>
+			<br>
+			<br>
+			
+				 <p class="w3-center w3-large">Vision<br>
+				Knowledge of tomorrow, ready for your learning today<br>
+			<br>
+			<br>
+
+				<p class="w3-center w3-large">Mission<br>
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;คอมพิวเตอร์เป็นศาสตร์ที่มีผลวัตร ภาควิชาวิศวกรรมคอมพิวเตอร์จึงปรับตัวอยู่เสมอ เพื่อให้นักศึกษาทุกคน
+				พร้อมที่จะออกไปทำงานจริงในอุตสาหกรรม <br>   นอกจากนั้นทีมนักวิจัยที่ได้รับการยอมรับในระดับนานาชาติภาควิชาวิศวกรรมคอมพิวเตอร์ได้นำเอาความชำนาญเข้ามาปรับปรุงหลักสูตรการเรียนการสอน<br>และบริการ
+				วิชาการให้กับทั้งภาครัฐและเอกชนในประเทศไทย<br>
+				<br>
+				<br>
+
+				<p class="w3-center w3-large">Curriculum<br>
+
+				&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;ภาควิชาวิศวกรรมคอมพิวเตอร์ มุ่งเน้นการเตรียมความพร้อมของนักศึกษาที่จะจบไปทำงานในอุตสาหกรรมได้ทันที
+				โดยมีความร่วมมือกับบริษัทและหน่วยงานต่างๆ <br>เพื่อที่จะทำการปรับปรุงหลักสูตรของภาควิชา ให้มีความทันสมัยอยู่เสมอหลักสูตรของภาควิชา ประกอบไปด้วยวิชาพื้นฐานทางวิศวกรรมคอมพิวเตอร์ และวิชาเลือก<br>&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;ตามความสนใจของนักศึกษาในระดับบัณฑิตศึกษา ได้เปิดสอนทั้งแบบภาคปกติกและภาคพิเศษ เพื่อตอบสนองความต้องการของผู้ที่ต้องการต่อยอดความรู้จากระดับ<br>ปริญญาตรี ปริญญาโท หรือผู้เรียนที่ทำงานแล้วต้องการเพิ่มโอกาสในหน้าที่การงาน<br>
+			<html>
+				<head>
+					<title>
+              
+					</title>
+				</head>
+			<body>
+			
+            <p class="w3-center w3-large">โครงสร้างภาควิชา<br>
+            <img src="CPE.jpg">
+			</body>
+			</html>
+			 
+			 
+			 </BODY>
+			
+			
+			
+			
 			<div class="w3-row-padding w3-center" style="margin-top:64px">
 				<div class="w3-quarter">
 					<i class="fa fa-desktop w3-margin-bottom w3-jumbo w3-center"></i>
