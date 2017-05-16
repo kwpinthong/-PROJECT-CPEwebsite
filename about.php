94e6907454b79c6ded7cec8295faeef32b81707f
ASUS_kwpinthong <kw.pinthong@gmail.com>
2017-05-17 00:27:48 +0700

    about

--- about.php
+++ about.php
@@ -1,22 +1,21 @@
 <?php require_once("header.php"); ?>
 
 	<!-- Header with full-height image -->
-		<header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
-			<div class="w3-display-Topleft w3-text-white" style="padding:48px">
-				<span class="w3-jumbo w3-hide-small"><b>Department of Computer Engineering </font></b></span><br>
-				<span class="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br>
-				<span class="w3-large"><font size = "8"> ChiangMai University </font></span>
-				
-			</div> 
-			<div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
-				<i class="fa fa-facebook-official w3-hover-opacity"></i>
-				<i class="fa fa-instagram w3-hover-opacity"></i>
-				<i class="fa fa-snapchat w3-hover-opacity"></i>
-				<i class="fa fa-pinterest-p w3-hover-opacity"></i>
-				<i class="fa fa-twitter w3-hover-opacity"></i>
-				<i class="fa fa-linkedin w3-hover-opacity"></i>
-			</div>
-		</header>
+	<header class="bgimg-0 w3-display-container w3-grayscale-min" id="home">
+		<div class="w3-display-left w3-text-white" style="padding:48px">
+			<span class="w3-jumbo w3-hide-small">Computer Enginerring Faculty of Enginerring Chiang mai University.</span><br>
+			<span class="w3-xxlarge w3-hide-large w3-hide-medium">Computer Enginerring Faculty of Enginerring Chiang mai University.</span><br>
+			<span class="w3-large">ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวะกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่</span>
+		</div> 
+		<div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
+			<i class="fa fa-facebook-official w3-hover-opacity"></i>
+			<i class="fa fa-instagram w3-hover-opacity"></i>
+			<i class="fa fa-snapchat w3-hover-opacity"></i>
+			<i class="fa fa-pinterest-p w3-hover-opacity"></i>
+			<i class="fa fa-twitter w3-hover-opacity"></i>
+			<i class="fa fa-linkedin w3-hover-opacity"></i>
+		</div>
+	</header>
 
 	<!--CONTENT-->
 		<div class="w3-container" style="padding:128px 16px" id="about">
